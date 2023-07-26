39d6e19591462b504d03e642d42e795fdcfa340b
Sara Paguaga <64711979+MGonza20@users.noreply.github.com>
2023-07-26 14:59:06 -0600

    Login done

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const { client, xml } = require("@xmpp/client");
-// const debug = require("@xmpp/debug");
+const readline = require('readline-sync');
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+let username = readline.question('Ingresa tu usuario: ');
+let password = readline.question('Ingresa tu contraseña: ', {
+  hideEchoBack: true
+});
 
 const xmpp = client({
   service: "xmpp://alumchat.xyz",
   domain: "alumchat.xyz",
   resource: "example",
-  username: "test",
-  password: "1234",
+  username: username,
+  password: password,
 });
-
-// debug(xmpp, true);
 
 xmpp.on("error", (err) => {
   console.error(err);
@@ -25,7 +28,7 @@ xmpp.on("stanza", async function stanzaHandler(stanza) {
     if (stanza.is("message")) {
         const body = stanza.getChildText("body");
         if (body) {
-            console.log(`New message from ${stanza.attrs.from}: ${body}\n`);
+            console.log(`Nuevo mensaje de: ${stanza.attrs.from}: ${body}\n`);
         }
     }
 });  
